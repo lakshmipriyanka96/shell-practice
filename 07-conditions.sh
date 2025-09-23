153862a649ce09d02d8d53202ff0978e4e49ec79
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-23 19:09:10 +0530

    upto conditions are updatd

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -5,6 +5,11 @@ NUMBER=$1
 if [ $NUMBER -lt 20 ]; then
 
 		echo "given  number $NUMBER is less than 20"
+	
+		elif [ $NUMBER -eq 20 ]; then
+
+		echo "given number $NUMBER is equal to 20"
 	else
-		echo "given number $NUMBER is greater than or equal to 20"
+		echo "given number $NUMBER is greater 20"
 fi
+
