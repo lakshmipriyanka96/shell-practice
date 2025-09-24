b248393b69532c0e414eae6fd36d0308cbc4307e
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-24 14:12:01 +0530

    script file updated

--- 09-script.sh
+++ 09-script.sh
@@ -3,11 +3,17 @@ USERID=$(id -u)
 if [ "$USERID" -ne 0 ]; then
   echo "Please run as root"
   exit 1
-  fi
-  dnf install mysql -y
-  if [ $? -ne 0 ]; then
-    echo "MySQL installation failed"
+  fi    
+  validate() { #functions recieve input through args just like shell scripting args
+if [ $1 -ne 0 ]; then
+    echo "$2 installation failed"
     else
-        echo "MySQL installed successfully"
-exit 1
-    fi
+        echo "$2 installed successfully"
+  fi
+  }
+  dnf install mysql -y
+  validate $? "$2"
+  dnf install nginx -y
+  validate $? "nginx"
+  dnf install mongodb-mongosh -y
+  validate $? "mongodb"
