20d5365eeacacd931a868067edc1fae8633206b0
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-24 15:37:52 +0530

    script file updated

--- 09-script.sh
+++ 09-script.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 USERID=$(id -u)
+G='\e[32m'
+R='\e[31m'
+NC='\e[0m' # No Color
+
 if [ "$USERID" -ne 0 ]; then
   echo "Please run as root"
   exit 1
   fi    
   validate() { #functions recieve input through args just like shell scripting args
 if [ $1 -ne 0 ]; then
-    echo "$2 installation failed"
+    echo -e "$R $2 installation failed $N "
     else
-        echo "$2 installed successfully"
+        echo -e "$G $2 installed successfully $N"
   fi
   }
-  dnf install mysql -y
+  dnf install mysql 
+  if [$? -ne 0 ]; then
+    dnf install mysql -y
+    vaidate $? "mysql"
+    else
+      echo -e "mysql already installed $Y..skipping $N"
+  fi
+dnf list installed nginx
+if [$? -eq 0 ]; then
+    dnf install nginx -y
+    validate $? "nginx"
+    else
+      echo -e "nginx already installed $Y..skipping $N"
+fi
+dnf list installed python3
+if [$? -eq 0 ]; then
+    dnf install python3 -y
+    validate $? "python3"
+    else
+      echo -e "python3 already installed $Y..SKIPPING $N" 
   validate $? "$2"
-  dnf install nginx -y
-  validate $? "nginx"
-  dnf install python3 -y
-  validate $? "python3"
+fi
