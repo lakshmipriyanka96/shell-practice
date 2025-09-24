7bf9f7467ee8ecac1e25ba58f8e01c376578e965
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-24 12:51:10 +0530

    script file updated

--- 09-script.sh
+++ 09-script.sh
@@ -2,10 +2,12 @@
 USERID=$(id -u)
 if [ "$USERID" -ne 0 ]; then
   echo "Please run as root"
+  exit 1
   fi
   dnf install mysql -y
   if [ $? -ne 0 ]; then
     echo "MySQL installation failed"
     else
         echo "MySQL installed successfully"
+exit 1
     fi
