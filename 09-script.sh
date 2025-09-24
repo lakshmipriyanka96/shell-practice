73d977583779bfecef1f4616f2c49138681b99dd
Priyanka <llakshmipriyanka1996@gmail.com>
2025-09-24 15:42:07 +0530

    script file updated

--- 09-script.sh
+++ 09-script.sh
@@ -16,21 +16,21 @@ if [ $1 -ne 0 ]; then
   fi
   }
   dnf install mysql 
-  if [$? -ne 0 ]; then
+  if [ $? -ne 0 ]; then
     dnf install mysql -y
     vaidate $? "mysql"
     else
       echo -e "mysql already installed $Y..skipping $N"
   fi
 dnf list installed nginx
-if [$? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     dnf install nginx -y
     validate $? "nginx"
     else
       echo -e "nginx already installed $Y..skipping $N"
 fi
 dnf list installed python3
-if [$? -eq 0 ]; then
+if [ $? -eq 0 ]; then
     dnf install python3 -y
     validate $? "python3"
     else
